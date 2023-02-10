788f6ff353ac02cec8e34c211c4558be9a3f4447
japo04001 <japo04001@utb.vaxjo.se>
2023-02-10 10:25:04 +0100

    Add project files.

---
+++ abcdef/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett andra tal");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Det största talet är {Störst(tal1, tal2)}");
+
+        }
+
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+
+            else if (tal2 > tal1)
+            {
+                return tal2;
+            }
+
+            else
+            {
+                return tal1;
+            }
+        }
+    }
+}
